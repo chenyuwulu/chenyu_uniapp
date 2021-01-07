143497af0e63ceb4c0f2135526b9f68dbc6e4093
chencailong <chencailong@chargefrog.com>
2021-01-07 19:03:10 +0800

    简单修改下，准备后期

--- uniapp_project/store/index.js
+++ uniapp_project/store/index.js
@@ -4,16 +4,16 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-	state: {//vuex的变量池
-		vuex_tabbar: [{
+	state: {
+		vuex_tabbar: [//uview的tabbar
+			{	text: '首页',
 				iconPath: "/static/tabbar/home.png",
 				selectedIconPath: "/static/tabbar/home-fill.png",
-				text: '首页',
 				pagePath: '/pages/index/index'
-			},{
+			},
+			{	text: '我的',
 				iconPath: "/static/tabbar/user.png",
 				selectedIconPath: "/static/tabbar/switch-user.png",
-				text: '我的',
 				pagePath: '/pages/user/index',
 			}
 		],
