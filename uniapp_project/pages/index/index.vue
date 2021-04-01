d40ab78e0a2a38b3f265cf5c2aaf907b0a21f27d
尘雨 <675687771@qq.com>
2021-04-01 10:45:25 +0800

    简单调整

--- uniapp_project/pages/index/index.vue
+++ uniapp_project/pages/index/index.vue
@@ -17,17 +17,13 @@
 				list: null
 			}
 		},
+		watch:{},
 		computed:{
-			vuex_tabbar(){
+			vuex_tabbar(){//这是底部tabbar
 				return this.$store.state.vuex_tabbar
 			}
 		},
 		onLoad(options) {
-			/*
-			直接共用request，充分利用ifdef,样例中特意把h5版本和其他版本做了方法不同的区分。
-			在实际使用中，全平台的方法名大多都是统一的，所以在写逻辑代码的时候，甚至都不需要区分平台方法
-			！！！注意在config,siteinfo配置中详细配置区分！！！
-			*/
 			this.$u.post('app/index.php',{//直接使用uview的封装方法来调用
 				// #ifdef MP-WEIXIN || MP-ALIPAY || MP-TOUTIAO || APP-PLUS
 					do:'test',
@@ -40,6 +36,8 @@
 			})
 		},
 		onShow(){},
+		onHide(){},
+		onUnload(){},
 		methods: {
 			
 		}
