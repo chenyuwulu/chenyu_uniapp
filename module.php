426b97a381b2b1e2e923879a80403f4d5febf471
尘雨 <675687771@qq.com>
2021-04-02 18:06:40 +0800

    进行调整

--- module.php
+++ module.php
@@ -1,14 +1,9 @@
 <?php
 /**
  * chenyu_uniapp模块定义
- *
- * @author 尘雨雾录
- * @url
+ * 如果有需要自定义微信公众号自动回复的，需要在这里进行开发配置
+ * 作者：尘雨雾录
  */
 defined('IN_IA') or exit('Access Denied');
 
-class Chenyu_uniappModule extends WeModule {
-
-
-
-}
+class Chenyu_uniappModule extends WeModule {}
---
+++ uniapp_project/pages/update_log.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="chenyu_page page_box">
+		1111
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		watch:{},
+		computed:{},
+		onLoad(options) {},
+		onShow(){},
+		onHide(){},
+		onUnload(){},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.page_box {
+	align-items: center;
+	.logo{
+		width: 200rpx;
+		height: 200rpx;
+	}
+	.content{
+		padding-left: 36rpx;
+		padding-right: 36rpx;
+		.title {
+			font-size: 34rpx;
+			color: #8f8f94;
+		}
+	}
+}
+</style>
